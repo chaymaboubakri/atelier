f7a35fc23e97c22fb6daee1f12ec9b2394054aa4
chaymaboubakri <96527065+chaymaboubakri@users.noreply.github.com>
2021-12-22 22:50:46 +0100

    Update client.php

--- client.php
+++ client.php
@@ -21,30 +21,51 @@
     </tr>
     
 <?php
+//insertion
 
-include("connexion.php)
+include("connexion.php")
+if(isset($_POST["idc"],$_POST["nomc"],
+$_POST["prenomc"],$_POST["tel"],$_POST["adress"],
+$_POST["numc"],$_POST["DDL"]))
+{
+    $rq="INSERT INTO 'client`('idc`, 'nomc', 'prenomc', 'tel', 'adress', 'numc', 'DDL') VALUES (:idc,:nomc,:prenomc,:tel,:adress,:numc,:DDL)";
+    $pr=$connexion->prepare($rq);
 
+    $tab1=array("idc"=>$_POST["idc"],"nomc"=>$_POST["nomc"],"prenomc"=>$_POST["prenomc"],
+    "tel"=>$_POST["tel"],"adress"=>$_POST["adress"],
+    "numc"=>$_POST["nmc"],"DDL"=>$_POST["DDL"]);
+
+    $EX=$pr->execute($tab1);
+}
  foreach($tab as $C) echo"
        <tr>
             
-            <td>". $C["nomprenom"] ." </td>
+            <td>". $C["nomc"] ." </td>
+            <td>". $C["prenomc"] ." </td>
             <td>" . $C["tel"]." </td>
-            <td> ". $C["adresse"] ." </td>
-            <td>". $C["numC"] ." </td>
-            <td>". $C["DDLplutot"] ." </td>
-            <td>". $C["DDLplutard"] ." </td>
+            <td> ". $C["adress"] ." </td>
+            <td>". $C["numc"] ." </td>
+            <td>". $C["DDL"] ." </td>
+        
             
             
-            <td> mise à jour  </td>
-            <td>supprimer </td>
+           
+	
+            <td><a href="client.php?id=<?=$c["id"];?>">Delete</a></br>"
+            <a href="client.php?id=<?=$c["id"];?>">Update</a></br>"
+    
+        </td>
+
+</td>
         </tr>"
 
     
-    ;
+    
+;
     ?>
    
 
 </table>
-    
+<a href='formulaireajoutc.php'>Ajouter</a>
 </body>
 </html>
