e5738158bfb2be02fa0dcfd02cc385bacd27d53e
chaymaboubakri <96527065+chaymaboubakri@users.noreply.github.com>
2021-12-22 14:35:44 +0100

    Update machine.php
    insertion

--- machine.php
+++ machine.php
@@ -18,29 +18,42 @@
     <td> </td>
 
 </tr>
+
     
 <?php
+//inserction
 include("connexion.php");
+if(isset($_POST["nummachine"],$_POST["dateAchat"],
+$_POST["cout"],$_POST["fonctionnement"],$_POST["ido"],))
+{
+    $rq="INSERT INTO 'machine`('nummachine`, 'dateAchat', 'cout', 'fonctionnement', 'ido') VALUES (:nummachine,:dateAchat,:cout,:fonctionnement,:ido)";
+
+    $pr=$connexion->prepare($rq);
+
+    $tab1=array("nummachine"=>$_POST["nummachine"],"datAchat"=>$_POST["dateAcht"],"cout"=>$_POST["cout"],
+    "fonctionnement"=>$_POST["fonctionnement"],"ido"=>$_POST["ido"]);
+
+    $EX=$pr->execute($tab1);
+}
 
 
- foreach($tab as $M) echo"
+
+ foreach($tab1 as $M) echo"
        <tr>
             <td>".$M["nummachine"]." </td>
             <td>". $M["`dateAchat`"] ." </td>
             <td>" . $M["cout"]." </td>
             <td> ". $M["fonctionnement"] ." </td>
             <td>". $M["ido"] ." </td>
-        </tr>"
-
-    
-    ;
+            <td><a href='update1.php?ido=$nummachine'>modifier</a> </td>
+            <td><a href='deleteo.php?ido=$nummachine'>supprimer</a> </td>
+        </tr>" ;
     ?>
    
 
 </table>
+<a href='formulaireajoutm.php'>Ajouter</a>
 
-
- 
     
 </body>
 </html>
